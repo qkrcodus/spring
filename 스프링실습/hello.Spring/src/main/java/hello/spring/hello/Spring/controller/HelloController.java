e74ac3f69853069e97a0a5b646a69deead67f908
qkrcodus <qkrcodus0722@gmail.com>
2025-01-12 16:46:56 +0900

    프로젝트 환경설정

---
+++ 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/controller/HelloController.java
@@ -0,0 +1,13 @@
+package hello.spring.hello.Spring.controller;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+       model.addAttribute("data","hello!!");
+       return "hello";
+    }
+}
