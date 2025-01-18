6a59e35408188c29dbaa13257099876f034a291e
qkrcodus <qkrcodus0722@gmail.com>
2025-01-18 14:44:57 +0900

    순수 JDBC

--- 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/domain/Member.java
+++ 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/domain/Member.java
@@ -1,6 +1,15 @@
 package hello.spring.hello.Spring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
     private Long id;
     public Long getId() {
--- 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/service/MemberService.java
+++ 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/service/MemberService.java
@@ -3,13 +3,15 @@ package hello.spring.hello.Spring.service;
 import hello.spring.hello.Spring.domain.Member;
 import hello.spring.hello.Spring.repository.MemberRepository;
 import hello.spring.hello.Spring.repository.MemoryMemberRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
