864c51309135333bbbdbc35496f5e4916d99a53f
qkrcodus <qkrcodus0722@gmail.com>
2025-01-31 23:43:11 +0900

    주문과 할인 도메인 개발

---
+++ 스프링실습/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository=new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 주문 요청이 오면 회원을 조회하고
+        Member member = memberRepository.findById(memberId);
+
+        // 회원을 통으로 넘긴다
+        int discountPrice=discountPolicy.discount(member,itemPrice);
+
+        return new Order(memberId,itemName,itemPrice,discountPrice);
+    }
+}
