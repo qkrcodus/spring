d76d67b7939ca0514ea5f98dccf9986581f5d825
qkrcodus <qkrcodus0722@gmail.com>
2025-02-03 22:42:42 +0900

    스프링으로 전환하기

--- 스프링실습/core/src/main/java/hello/core/AppConfig.java
+++ 스프링실습/core/src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     // 할인 정책을 교체할 수 있다.
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
@@ -30,6 +37,7 @@ public class AppConfig {
     }
 
 }
+
 
 
 
