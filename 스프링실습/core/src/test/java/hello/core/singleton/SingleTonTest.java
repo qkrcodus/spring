da172f6626e2aad75a92a6a2045e2cf64c92b167
qkrcodus <qkrcodus0722@gmail.com>
2025-02-04 14:38:39 +0900

    싱글톤 컨테이너

--- 스프링실습/core/src/test/java/hello/core/singleton/SingleTonTest.java
+++ 스프링실습/core/src/test/java/hello/core/singleton/SingleTonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingleTonTest {
 
@@ -35,5 +37,19 @@ public class SingleTonTest {
         
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
     }
-    
+
+    @Test
+    @DisplayName("싱글톤 패턴을 사용한 객체 사용")
+    void springContainer(){
+        ApplicationContext ac=new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1=ac.getBean(MemberService.class);
+        MemberService memberService2=ac.getBean(MemberService.class);
+
+        //참조값이 다르다면 요청할때마다 새로운 객체를 생성한다는 뜻임
+        System.out.println("memberService1="+memberService1);
+        System.out.println("memberService2="+memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+
 }
