8ec12a8ad98bfeffcaa7fd3b873f1c4b135abee6
qkrcodus <qkrcodus0722@gmail.com>
2025-02-08 13:28:18 +0900

    view 환경 설정

--- 스프링실습/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ 스프링실습/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-
 		Hello hello = new Hello();
 		hello.setData("hihi");
 		String data=hello.getData();
