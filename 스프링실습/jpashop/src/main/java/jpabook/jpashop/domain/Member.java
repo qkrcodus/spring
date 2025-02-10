b2d31f3ad629e6d5977bc43a70c742f52dcc99f9
qkrcodus <qkrcodus0722@gmail.com>
2025-02-10 21:24:19 +0900

    엔티티 클래스 개발1

---
+++ 스프링실습/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 여기에 뭘 넣는다고 해서 fk가 바뀌진 않는다.
+    private List<Order> orders= new ArrayList<>();
+
+}
+
+
