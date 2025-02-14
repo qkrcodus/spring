822c9fa1843c632dcd48c8e1fcf8a26364b07b6b
qkrcodus <qkrcodus0722@gmail.com>
2025-02-14 23:37:57 +0900

    상품 리포지토리 개발

---
+++ 스프링실습/jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jpabook.jpashop.domain.item.Item;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class ItemRepository {
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            // 처음엔 item 은 jpa 에 저장하기 전에 id가 없다.
+            // 새로운 객체 만들때
+            em.persist(item);
+        }else{
+            // 이미 만들어진 아이템
+            em.merge(item);
+        }
+    }
+
+    // 1개 찾는건 find
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    // 여러개 찾는 건 jpql
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
+
+
+
