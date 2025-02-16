239bd2cc3e828f149451b40e1eeb9561fc621085
qkrcodus <qkrcodus0722@gmail.com>
2025-02-16 21:04:23 +0900

    회원 목록 조회

--- 스프링실습/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ 스프링실습/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -14,7 +14,6 @@ public class Member {
     @Id @GeneratedValue // 기본 키를 데이터베이스가 자동 생성
     @Column(name="member_id") // 테이블의 해당 컬럼 이름 설정
     private Long id;
-
     private String name;
 
     @Embedded // 복합 타입 값을 하나의 엔티티에 넣을 때
