8363fd79127efd8bdda71c8f66e96cd5477247b1
qkrcodus <qkrcodus0722@gmail.com>
2025-03-02 23:02:26 +0900

    스프링 데이터 JPA

--- 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/SpringConfig.java
+++ 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/SpringConfig.java
@@ -13,21 +13,18 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    private final EntityManager em;
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository=memberRepository;
     }
+
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-    @Bean
-    public MemberRepository memberRepository() {
-// return new MemoryMemberRepository();
-return new JdbcMemberRepository(dataSource);
-// return new JdbcTemplateMemberRepository(dataSource);
-        //return new JpaMemberRepository(em);
-    }
+
 }
--- 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/repository/SpringDataJpaMemberRepository.java
+++ 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/repository/SpringDataJpaMemberRepository.java
@@ -1,4 +1,13 @@
 package hello.spring.hello.Spring.repository;
 
-public interface SpringDataJpaMemberRepository {
+import hello.spring.hello.Spring.domain.Member;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
 }
