b9e44d23d57ed22871e6187f2af00cd17d431371
qkrcodus <qkrcodus0722@gmail.com>
2025-03-02 23:44:55 +0900

    AOP가 필요한 상황

--- 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/repository/JpaMemberRepository.java
+++ 스프링실습/hello.Spring/src/main/java/hello/spring/hello/Spring/repository/JpaMemberRepository.java
@@ -9,10 +9,17 @@ import java.util.Optional;
 public class JpaMemberRepository implements MemberRepository {
 
     private final EntityManager em;
-
     public JpaMemberRepository(EntityManager em) {
         this.em = em;
     }
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> members = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return members.stream().findAny();
+    }
+
 
     @Override
     public Member save(Member member) {
@@ -26,13 +33,7 @@ public class JpaMemberRepository implements MemberRepository {
         return Optional.ofNullable(member);
     }
 
-    @Override
-    public Optional<Member> findByName(String name) {
-        List<Member> members = em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-        return members.stream().findAny();
-    }
+
 
     @Override
     public List<Member> findAll() {
